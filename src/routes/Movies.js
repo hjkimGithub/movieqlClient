da765f1fe9551eac90c32451bcf684894c7914db
김현재 <rlaguswo3408@gmail.com>
2022-12-28 20:26:05 +0900

    useQuery 훅의 힘

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,29 +1,25 @@
-import { gql, useApolloClient } from "@apollo/client"
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client"
+
+const ALL_MOVIES = gql`
+    query getMovies {
+        allMovies {
+            title
+            id
+        }
+    }
+`;
 
 export default function Movies() {
-    const [movies, setMovies] = useState([]);
-    const client = useApolloClient();
-
-    useEffect(() => {
-        client.query({
-            query: gql `
-                {
-                    allMovies {
-                        title
-                        id
-                    }
-                }
-            `,
-        })
-        .then((results) => setMovies(results.data.allMovies));
-    }, [client]);
-
+    const {data, loading, error} = useQuery(ALL_MOVIES);
+    if(loading) {
+        return <h1>Loading...</h1>
+    }
+    if(error) {
+        return <h1>Could not fetch</h1>
+    }
     return (
         <ul>
-            {movies.map((movie) => (
-                <li key={movie.id}>{movie.title}</li>
-            ))}
+            {data.allMovies.map(movie => <li key={movie.id}>{movie.title}</li>)}
         </ul>
     );
 }
